a31bd78a5fd1e95fbb7ac7fac014761f731e8a0f
Lisa Lee <llee28@cnm.edu>
2019-10-15 14:27:18 -0600

    added ERD

---
+++ epic/erd.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>ERD</title>
+</head>
+<body>
+<h1 class="center"><strong>Entity Relationship Diagram</strong></h1>
+<div>
+	<img src="madeleine.jpg">
+</div>
+<div>
+	<img src="Epic-Data-Design-ERD-10-14.jpg">
+</div>
+</body>
+</html>
